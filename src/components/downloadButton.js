40b5c4eeeb0453d6900749a0975fab527fb2f3ce
cailey <caileymillerit@gmail.com>
2020-08-31 18:43:46 -0400

    fixed ramda v26 bug breaking asy async in safari

--- src/components/downloadButton.js
+++ src/components/downloadButton.js
@@ -5,16 +5,27 @@ import PDFOutput from "./pdfOutput.js";
 import { Button } from "@chakra-ui/core";
 import { PDFDocument } from "pdf-lib";
 
+function getArrayBuffer (blob) {
+    //seemingly can't use native arrayBuffer() in safari, the cursed problem child of 2020. 
+    return new Promise((resolve) => {
+      let fr = new FileReader();
+      fr.onload = () => {
+        resolve(fr.result);
+      };
+      fr.readAsArrayBuffer(blob);
+    })
+}
+
 export const DownloadButton = () => (
   <Button
     onClick={async () => {
+      //workaround for a toBlob bug in @react-pdf V2 beta. Awaiting a fix.
       const doc = <PDFOutput />;
       const asPdf = pdf({}); //important, throws without an argument
       asPdf.updateContainer(doc);
       let blob = await asPdf.toBlob();
-      //To download only first page: convert blob to buffer to use pdf-lib's removePage functionality.
-      //There's probably a better way to do this but this works for now.
-      let buffer = await blob.arrayBuffer();
+      //convert to buffer to delete possible overflow pages. 
+      let buffer = await getArrayBuffer(blob);
       const pdfDoc = await PDFDocument.load(buffer);
       const pageCount = await pdfDoc.getPageCount();
       if (pageCount > 1) {
